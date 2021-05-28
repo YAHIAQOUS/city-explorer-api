aaf3ccce5fc6daff8e210ef1c2441af038443e2c
Yahia Qous <yahiaqous@gmail.com>
2021-05-28 20:02:17 +0300

    test server

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ server.get('/test', (req, res) => {
 
 
 // lab08
-// http://localhost:3006/weather?city=zarqa
+// http://localhost:3006/weather?city=amman
 // http://api.weatherbit.io/v2.0/current?&city=zarqa&key=API_KEY
 server.get('/weather', weatherHandler)
 
@@ -67,13 +67,49 @@ function weatherHandler(req, res) {
             console.log(cityData);
             res.send(cityData);
         })
-
         .catch(err => {
             console.log('inside error');
             res.status(500).send(`error in getting data ==> ${err}`)
         })
+}
 
 
+// http://localhost:3006/movies?city=amman
+// https://api.themoviedb.org/3/search/movie?api_key=2f572ab958bab1048cb95a108203162a&query=amman
+server.get('/movies', moviesHandler)
+
+function moviesHandler(req, res) {
+    let cityQuery = req.query.city;
+    // let key = process.env.API_KEY;
+    let key = '2f572ab958bab1048cb95a108203162a';
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query==${cityQuery}`
+
+    axios
+        .get(url)
+        .then(result => {
+            console.log('inside promise');
+            let cityData = result.data.results.map(movieItem => {
+                return new Movie(movieItem)
+            })
+            console.log(cityData);
+            res.send(cityData);
+        })
+        .catch(err => {
+            console.log('inside error');
+            res.status(500).send(`error in getting data ==> ${err}`)
+        })
+}
+
+class Movie {
+    constructor(item) {
+        this.title = item.title,
+            this.overview = item.overview,
+            this.average_votes = item.vote_average,
+            this.total_votes = item.vote_count,
+            this.image_url = item.poster_path,
+            this.popularity = item.popularity,
+            this.released_on = item.release_date
+    }
 }
 
 
@@ -82,8 +118,6 @@ function weatherHandler(req, res) {
 server.get('*', (req, res) => {
     res.send('Error: Something went wrong.');
 })
-
-
 
 server.listen(PORT, () => {
     console.log('Server Listining');
