9729b8dee21913e272293a4b0741316a8581e4fc
Yahia Qous <yahiaqous@gmail.com>
2021-05-28 17:25:24 +0300

    lab08 Done

--- server.js
+++ server.js
@@ -21,48 +21,61 @@ server.get('/test', (req, res) => {
 
 // lab07
 // http://localhost:3006/weather?cityName=amman
-server.get('/weather', (req, res) => {
+// server.get('/weather', (req, res) => {
 
-    let weatherItem = JSONData.find(item => {
-        if (item.city_name.toLowerCase() == req.query.cityName)
-            return item;
-    })
+//     let weatherItem = JSONData.find(item => {
+//         if (item.city_name.toLowerCase() == req.query.cityName)
+//             return item;
+//     })
 
-    let weatherArray = [];
-    for (let i = 0; i < weatherItem.data.length; i++) {
-        let cityData = {
-            description: weatherItem.data[i].weather.description,
-            date: weatherItem.data[i].valid_date
-        }
-        weatherArray.push(cityData)
-    }
+//     let weatherArray = [];
+//     for (let i = 0; i < weatherItem.data.length; i++) {
+//         let cityData = {
+//             description: weatherItem.data[i].weather.description,
+//             date: weatherItem.data[i].valid_date
+//         }
+//         weatherArray.push(cityData)
+//     }
 
-    res.status(200).send(weatherArray);
-})
+//     res.status(200).send(weatherArray);
+// })
 
 
 
 // lab08
+// http://localhost:3006/weather?city=zarqa
 // http://api.weatherbit.io/v2.0/current?&city=zarqa&key=API_KEY
-server.get('/', weatherHandler)
+server.get('/weather', weatherHandler)
 
 function weatherHandler(req, res) {
-    let key = process.env.UNSPLASH_KEY;
-    let url = `http://api.weatherbit.io/v2.0/current?&city=${req.query.searchQuery}&key=${key}`
+    let cityQuery = req.query.city;
+    let key = process.env.API_KEY;
+    let url = `http://api.weatherbit.io/v2.0/current?city=${cityQuery}&key=${key}`
 
     axios
         .get(url)
         .then(result => {
-            console.log(result);
-            // const weatherArray = result.data.results.map()
+            console.log('inside promise');
+            let cityData = {
+                description: result.data.data[0].weather.description,
+                solarRad: result.data.data[0].solar_rad,
+                windSpd: result.data.data[0].wind_spd,
+                windDir: result.data.data[0].wind_dir,
+                temp: result.data.data[0].temp
+            }
+            console.log(cityData);
+            res.send(cityData);
         })
 
-        .catch(err=>{
+        .catch(err => {
+            console.log('inside error');
             res.status(500).send(`error in getting data ==> ${err}`)
         })
 
 
 }
+
+
 
 
 server.get('*', (req, res) => {
