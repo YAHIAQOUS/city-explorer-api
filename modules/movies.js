dfe8eeb16dc0a982f8463b6615be8b0a56d6a051
Yahia Qous <yahiaqous@gmail.com>
2021-05-29 14:54:02 +0300

    lab10 Done

--- modules/movies.js
+++ modules/movies.js
@@ -7,6 +7,7 @@ module.exports = moviesHandler;
 // http://localhost:3006/movies?city=amman
 // https://api.themoviedb.org/3/search/movie?api_key=2f572ab958bab1048cb95a108203162a&query=amman
 
+let inMemory = {};
 
 function moviesHandler(req, res) {
     let cityQuery = req.query.city;
@@ -14,20 +15,45 @@ function moviesHandler(req, res) {
     // let key = '2f572ab958bab1048cb95a108203162a';
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query==${cityQuery}`
 
-    axios
-        .get(url)
-        .then(result => {
-            console.log('inside promise');
-            let cityData = result.data.results.map(movieItem => {
-                return new Movie(movieItem)
+    // lab09
+    // axios
+    //     .get(url)
+    //     .then(result => {
+    //         console.log('inside promise');
+    //         let cityData = result.data.results.map(movieItem => {
+    //             return new Movie(movieItem)
+    //         })
+    //         console.log(cityData);
+    //         res.send(cityData);
+    //     })
+    //     .catch(err => {
+    //         console.log('inside error');
+    //         res.status(500).send(`error in getting data ==> ${err}`)
+    //     })
+
+
+    if (inMemory[cityQuery] != undefined) {
+        console.log('Getting data from Memory');
+        // console.log(inMemory[cityQuery]);
+        res.send(inMemory[cityQuery]);
+    } else {
+        console.log('Getting data from API');
+        axios
+            .get(url)
+            .then(result => {
+                console.log('inside promise');
+                let cityData = result.data.results.map(movieItem => {
+                    return new Movie(movieItem)
+                })
+                inMemory[cityQuery] = cityData;
+                // console.log(cityData);
+                res.send(cityData);
             })
-            console.log(cityData);
-            res.send(cityData);
-        })
-        .catch(err => {
-            console.log('inside error');
-            res.status(500).send(`error in getting data ==> ${err}`)
-        })
+            .catch(err => {
+                console.log('inside error');
+                res.status(500).send(`error in getting data ==> ${err}`)
+            })
+    }
 }
 
 class Movie {
