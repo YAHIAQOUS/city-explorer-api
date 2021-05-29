4da2db8367374ceea9b7dbf7f224890f64593259
Yahia Qous <yahiaqous@gmail.com>
2021-05-29 11:11:52 +0300

    Lab09 Done

--- server.js
+++ server.js
@@ -22,12 +22,10 @@ server.get('/test', (req, res) => {
 // const JSONData = require('./data/weather.json');
 // http://localhost:3006/weather?cityName=amman
 // server.get('/weather', (req, res) => {
-
 //     let weatherItem = JSONData.find(item => {
 //         if (item.city_name.toLowerCase() == req.query.cityName)
 //             return item;
 //     })
-
 //     let weatherArray = [];
 //     for (let i = 0; i < weatherItem.data.length; i++) {
 //         let cityData = {
@@ -36,81 +34,88 @@ server.get('/test', (req, res) => {
 //         }
 //         weatherArray.push(cityData)
 //     }
-
 //     res.status(200).send(weatherArray);
 // })
 
+
+// lab09
+const weatherHandler = require('./modules/weather')
+server.get('/weather', weatherHandler)
 
 
 // lab08
 // http://localhost:3006/weather?city=amman
 // http://api.weatherbit.io/v2.0/current?&city=zarqa&key=API_KEY
-server.get('/weather', weatherHandler)
+// function weatherHandler(req, res) {
+//     let cityQuery = req.query.city;
+//     let key = process.env.WEATHER_KEY;
+//     // let key = '0744e560367343ae9d810f1723397ea4';
+//     let url = `http://api.weatherbit.io/v2.0/current?city=${cityQuery}&key=${key}`
 
-function weatherHandler(req, res) {
-    let cityQuery = req.query.city;
-    let key = process.env.WEATHER_KEY;
-    // let key = '0744e560367343ae9d810f1723397ea4';
-    let url = `http://api.weatherbit.io/v2.0/current?city=${cityQuery}&key=${key}`
-
-    axios
-        .get(url)
-        .then(result => {
-            console.log('inside promise');
-            let cityData = {
-                description: result.data.data[0].weather.description,
-                solarRad: result.data.data[0].solar_rad,
-                windSpd: result.data.data[0].wind_spd,
-                windDir: result.data.data[0].wind_dir,
-                temp: result.data.data[0].temp
-            }
-            console.log(cityData);
-            res.send(cityData);
-        })
-        .catch(err => {
-            console.log('inside error');
-            res.status(500).send(`error in getting data ==> ${err}`)
-        })
-}
+//     axios
+//         .get(url)
+//         .then(result => {
+//             console.log('inside promise');
+//             let cityData = {
+//                 description: result.data.data[0].weather.description,
+//                 solarRad: result.data.data[0].solar_rad,
+//                 windSpd: result.data.data[0].wind_spd,
+//                 windDir: result.data.data[0].wind_dir,
+//                 temp: result.data.data[0].temp
+//             }
+//             console.log(cityData);
+//             res.send(cityData);
+//         })
+//         .catch(err => {
+//             console.log('inside error');
+//             res.status(500).send(`error in getting data ==> ${err}`)
+//         })
+// }
 
 
-// http://localhost:3006/movies?city=amman
-// https://api.themoviedb.org/3/search/movie?api_key=2f572ab958bab1048cb95a108203162a&query=amman
+
+// lab09
+const moviesHandler = require('./modules/movies')
 server.get('/movies', moviesHandler)
 
-function moviesHandler(req, res) {
-    let cityQuery = req.query.city;
-    let key = process.env.MOVIES_KEY;
-    // let key = '2f572ab958bab1048cb95a108203162a';
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query==${cityQuery}`
 
-    axios
-        .get(url)
-        .then(result => {
-            console.log('inside promise');
-            let cityData = result.data.results.map(movieItem => {
-                return new Movie(movieItem)
-            })
-            console.log(cityData);
-            res.send(cityData);
-        })
-        .catch(err => {
-            console.log('inside error');
-            res.status(500).send(`error in getting data ==> ${err}`)
-        })
-}
+// lab08
+// http://localhost:3006/movies?city=amman
+// https://api.themoviedb.org/3/search/movie?api_key=2f572ab958bab1048cb95a108203162a&query=amman
 
-class Movie {
-    constructor(item) {
-        this.title = item.title,
-            this.overview = item.overview,
-            this.average_votes = item.vote_average,
-            this.total_votes = item.vote_count,
-            this.image_url = item.poster_path,
-            this.popularity = item.popularity,
-            this.released_on = item.release_date
-    }
-}
+// function moviesHandler(req, res) {
+//     let cityQuery = req.query.city;
+//     let key = process.env.MOVIES_KEY;
+//     // let key = '2f572ab958bab1048cb95a108203162a';
+//     let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query==${cityQuery}`
+
+//     axios
+//         .get(url)
+//         .then(result => {
+//             console.log('inside promise');
+//             let cityData = result.data.results.map(movieItem => {
+//                 return new Movie(movieItem)
+//             })
+//             console.log(cityData);
+//             res.send(cityData);
+//         })
+//         .catch(err => {
+//             console.log('inside error');
+//             res.status(500).send(`error in getting data ==> ${err}`)
+//         })
+// }
+
+// class Movie {
+//     constructor(item) {
+//         this.title = item.title,
+//             this.overview = item.overview,
+//             this.average_votes = item.vote_average,
+//             this.total_votes = item.vote_count,
+//             this.image_url = item.poster_path,
+//             this.popularity = item.popularity,
+//             this.released_on = item.release_date
+//     }
+// }
 
 
 
